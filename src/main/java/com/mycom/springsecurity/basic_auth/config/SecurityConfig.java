0f161576db7ba6d6d41260c732ac74de5847750d
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-03 16:35:10 +0900

    :memo: Security Config 메서드 정리

--- src/main/java/com/mycom/springsecurity/basic_auth/config/SecurityConfig.java
+++ src/main/java/com/mycom/springsecurity/basic_auth/config/SecurityConfig.java
@@ -21,10 +21,20 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 참고 사항 : 스프링 3.1.x 버전 부터 람다형식 표현 필수
         http
-                .authorizeRequests((auth)-> auth
+                .authorizeRequests((auth)-> auth // 이 authorizeRequest를 통해서 특정한 경로의 요청을 허용하거나 거부할 수 있습니다.
+                        // requestMatchers의 경우 작성한 경로에서 특정한 잡업을 하고 싶다는 설정을 진행할 수 있는 메서드입니다.
+                        // 각 메서드를 선언하고 이 메서드 뒤에 특정한 권한을 줄 수 있습니다.
+                        /*
+                        * permitAll : 모든 사용자에게 로그인을 하지 않아도 접근할 수 있는 메서드
+                        * hasRole : 특정한 규칙이 있어야 이 경로에 접근할 수 있습니다.
+                        * authenticated : 로그인만 진행하면 모두 접근할 수 있는 것입니다.
+                        * denyAll : 모든 사용자의 접근을 막는 메소드
+                        * */
                         .requestMatchers("/","login").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+
+                        // 위에서 처리하지 못한 나머지 경로들
                         .anyRequest().authenticated()
                 );
 
