e9ff5500be8656e19ad4259f061a075ead6100d2
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 19:35:10 +0900

    :recycle: JoinService에서 UserRole enum 사용하도록 수정

--- src/main/java/com/mycom/springsecurity/basic_auth/service/JoinService.java
+++ src/main/java/com/mycom/springsecurity/basic_auth/service/JoinService.java
@@ -1,12 +1,16 @@
 package com.mycom.springsecurity.basic_auth.service;
 
+import com.mycom.springsecurity.basic_auth.common.exception.UsernameAlreadyExistsException;
 import com.mycom.springsecurity.basic_auth.dto.JoinDTO;
 import com.mycom.springsecurity.basic_auth.entity.UserEntity;
+import com.mycom.springsecurity.basic_auth.entity.UserRole;
 import com.mycom.springsecurity.basic_auth.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional // 메소드 레벨의 트랜잭션을 보장
 public class JoinService {
 
     private final UserRepository userRepository;
@@ -19,23 +23,21 @@ public class JoinService {
 
     public void joinProcess(JoinDTO joinDTO) {
         // db에 이미 동일한 username을 가진 회원이 존재하는지
-        boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
-        if(isUser) {
-            return;
+        if(userRepository.existsByUsername(joinDTO.getUsername())) {
+            throw new UsernameAlreadyExistsException("이미 동일한 username이 있습니다. : "+joinDTO.getUsername());
         }
+        UserRole role = determineUserRole(joinDTO.getUsername());
 
-        UserEntity data = new UserEntity();
+        UserEntity user = UserEntity.builder()
+                .username(joinDTO.getUsername())
+                .password(bCryptPasswordEncoder.encode(joinDTO.getPassword()))
+                .role(role)
+                .build();
 
-        data.setUsername(joinDTO.getUsername());
-        data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
+        userRepository.save(user);
+    }
 
-        // admin 계정에 대해 ROLE_ADMIN 할당
-        if ("admin".equals(joinDTO.getUsername())) {
-            data.setRole("ROLE_ADMIN");
-        } else {
-            data.setRole("ROLE_USER");
-        }
-
-        userRepository.save(data);
+    private UserRole determineUserRole(String username) {
+        return "admin".equals(username) ? UserRole.ROLE_ADMIN : UserRole.ROLE_USER;
     }
 }
