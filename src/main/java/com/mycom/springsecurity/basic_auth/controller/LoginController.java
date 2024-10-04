5822e2e0020cc673e326063750d55c4f37b120e4
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 16:35:10 +0900

    :sparkles: 커스텀 로그인 컨트롤러 작성

---
+++ src/main/java/com/mycom/springsecurity/basic_auth/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.mycom.springsecurity.basic_auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
