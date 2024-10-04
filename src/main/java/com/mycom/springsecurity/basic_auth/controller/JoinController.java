26dbf9861877eb682d33b350b49fc46d06c90323
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 19:35:10 +0900

    :sparkles: JoinController 구현

---
+++ src/main/java/com/mycom/springsecurity/basic_auth/controller/JoinController.java
@@ -0,0 +1,28 @@
+package com.mycom.springsecurity.basic_auth.controller;
+
+import com.mycom.springsecurity.basic_auth.dto.JoinDTO;
+import com.mycom.springsecurity.basic_auth.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JoinController {
+
+    @Autowired
+    private JoinService joinService;
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @PostMapping("/joinProc")
+    public String joinProcess(JoinDTO joinDTO) {
+        System.out.println(joinDTO.getUsername());
+        joinService.joinProcess(joinDTO);
+        return "redirect:/login";
+    }
+    
+}
