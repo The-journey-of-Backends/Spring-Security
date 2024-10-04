5f2cf1627bc62fb3fe5b57433adea8e3d5f54640
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 19:35:10 +0900

    :sparkles: 유저 찾기를 통해 로그인 기능 구현

--- src/main/java/com/mycom/springsecurity/basic_auth/repository/UserRepository.java
+++ src/main/java/com/mycom/springsecurity/basic_auth/repository/UserRepository.java
@@ -4,4 +4,9 @@ import com.mycom.springsecurity.basic_auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    // 중복 검증
+    boolean existsByUsername(String username);
+
+    // 유저 찾기
+    UserEntity findByUsername(String username);
 }
