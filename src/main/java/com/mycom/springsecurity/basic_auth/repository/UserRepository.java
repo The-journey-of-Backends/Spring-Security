e38026bc7240694a18a6cec27e37e256b6924c43
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 19:35:10 +0900

    :recycle: Optional 방식으로 변경

--- src/main/java/com/mycom/springsecurity/basic_auth/repository/UserRepository.java
+++ src/main/java/com/mycom/springsecurity/basic_auth/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.mycom.springsecurity.basic_auth.repository;
 import com.mycom.springsecurity.basic_auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     // 중복 검증
     boolean existsByUsername(String username);
 
     // 유저 찾기
-    UserEntity findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
 }
