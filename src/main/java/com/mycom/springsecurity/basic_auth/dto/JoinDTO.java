59a4bd8ebd2509e65bb3a53e11baa68e822732d1
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 19:35:10 +0900

    :sparkles: JoinDto 구현

---
+++ src/main/java/com/mycom/springsecurity/basic_auth/dto/JoinDTO.java
@@ -0,0 +1,11 @@
+package com.mycom.springsecurity.basic_auth.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDTO {
+    private String username;
+    private String password;
+}
