f5499678ef0579dc817fb2f6463e22e46e051f01
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-10-04 19:35:10 +0900

    :sparkles: ROLE 역할 할당

--- src/main/java/com/mycom/springsecurity/basic_auth/service/JoinService.java
+++ src/main/java/com/mycom/springsecurity/basic_auth/service/JoinService.java
@@ -19,12 +19,22 @@ public class JoinService {
 
     public void joinProcess(JoinDTO joinDTO) {
         // db에 이미 동일한 username을 가진 회원이 존재하는지
+        boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
+        if(isUser) {
+            return;
+        }
 
         UserEntity data = new UserEntity();
 
         data.setUsername(joinDTO.getUsername());
         data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
-        data.setRole("ROLE_USER");
+
+        // admin 계정에 대해 ROLE_ADMIN 할당
+        if ("admin".equals(joinDTO.getUsername())) {
+            data.setRole("ROLE_ADMIN");
+        } else {
+            data.setRole("ROLE_USER");
+        }
 
         userRepository.save(data);
     }
